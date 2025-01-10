c2d1c0bd9c9e560b81516578aeab36575283cf37
LCB <cblee@laonpeople.com>
2025-01-10 12:53:41 +0900

    [UPDATE] 중첩 설명 추가

--- decorator.py
+++ decorator.py
@@ -1,9 +1,14 @@
+"""
+데코레이터는 함수를 인자로 받고 새로운 함수를 반환하는 함수
+함수를 감싸는 역활
+"""
+
 ### 1. 일반 함수 ###
 def hello(strA:str):
     print(f'hello {strA}')
     return "hello "+strA
 
-hello('LCB')
+# hello('LCB')
 
 ### 2. decorator 예시 ###
 def deco(func):
@@ -18,5 +23,23 @@ def deco(func):
 def say_hello(name):
     print(f'hello {name}')
 
-say_hello('LCB')
+# say_hello('LCB')
 
+### 3. 중첩 ###
+"""
+deco(deco_2(func)) 형태로 처리됨
+"""
+def deco_2(func):
+    def wrapper_func2(strA:str):
+        print("전처리2~")
+        result = func(strA)
+        print("후처리2~")
+        return result
+    return wrapper_func2
+
+@deco
+@deco_2
+def say_hello2(name):
+    print(f'hello {name}')
+
+say_hello2('LCB')
